66527b4fe7cbc2875bf6e6556cca533a3a0039fe
Ashay Ramolia <ashayramolia@gmail.com>
2022-05-11 13:27:30 +0530

    Add EJS and views Folder

--- app.js
+++ app.js
@@ -2,16 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 8069; // change it to environment variable when publishing server
-app.use(bodyParser.urlencoded({extended:true}))
+// app.use(bodyParser.urlencoded({extended:true}))
+app.set('view engine','ejs')
 
 app.get('/', (req, res) => {
     var today = new Date()
-    if (today.getDay()===6 || today.getDay()===0) { 
-        res.send(`Today seems to be a holiday, You should be chilling`)
-    } else {
-        res.sendFile(__dirname + '/index.html')
-    }
 
+    res.render('list', {date:today})
     // res.send('<h1>To-Do List</h1>')
 })
 
