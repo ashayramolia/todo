610ab681ee608cd3930342154afc778b903924a4
Ashay Ramolia <ashayramolia@gmail.com>
2022-05-11 13:08:01 +0530

    Add EJS

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 8069; // change it to environment variable when publishing server
+app.use(bodyParser.urlencoded({extended:true}))
 
+app.get('/', (req, res) => {
+    var today = new Date()
+    if (today.getDay()===6 || today.getDay()===0) { 
+        res.send(`Today seems to be a holiday, You should be chilling`)
+    } else {
+        res.sendFile(__dirname + '/index.html')
+    }
 
-app.listen(port, () => {
+    // res.send('<h1>To-Do List</h1>')
+})
+
+app.listen(process.env.PORT || port, () => {
     console.log(`Listening on port ${port}`);    
 })
